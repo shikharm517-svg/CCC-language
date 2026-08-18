5ffb798280fa1aeeab01b07381be4d0a047bf649
Shikhar Mishra <shikharm517@gmail.com>
2026-08-18 20:19:18 +0530

    explicit type

---
+++ explicit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double pi = 3.14;  
+
+    int value = (int)pi;  // double → int
+
+    cout << "Explicit Result: " << value << endl;
+    return 0;
+}
