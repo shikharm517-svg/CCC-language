176bdef92666c999fe83850ea47b4688719da01b
Shikhar Mishra <shikharm517@gmail.com>
2026-08-18 20:15:42 +0530

    implicit type done

---
+++ implicit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 7;
+    double y = 3.5;
+
+    //  conversion: int → double
+    double result = x + y;  
+
+    cout << "Implicit Conversion Result: " << result << endl;
+    return 0;
+}
