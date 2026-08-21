866508410abb3253a5d418840a3f4a1707b63eff
Shikhar Mishra <shikharm517@gmail.com>
2026-08-21 21:28:15 +0530

    sum

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int sum = a + b;
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
