fdc026dad6429bce450e0cbfe14592bea4d03e0f
Shikhar Mishra <shikharm517@gmail.com>
2026-08-22 13:33:09 +0530

    post increament

--- sum of two no.cpp
+++ sum of two no.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
 
 int a,b;
+
     cout <<" enter a";
     cin >> a ;
 
---
+++ unary operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+     
+    int a=10;
+    int b=a++; // post increament operator//
+
+     cout << "b=:" << b << endl;
+     cout << "a="  << a <<endl;
+
+     return 0;
+ }
