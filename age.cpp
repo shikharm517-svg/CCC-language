044bbead4a83b8af27600655cb91747d78b3293b
Shikhar Mishra <shikharm517@gmail.com>
2026-08-26 08:00:31 +0530

    if else condition

---
+++ age.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+   int main() {
+
+    int age = 23;
+
+    if(age>=18){
+
+        cout << "the person can vote:";
+    
+    }else{
+
+        cout << "the person can not vote:";
+    }
+        return 0;
+   }
