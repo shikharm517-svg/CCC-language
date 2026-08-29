36a8878289d967a4737fa7a7707e6a302f961397
Shikhar Mishra <shikharm517@gmail.com>
2026-08-29 13:21:13 +0530

    update grades checker

---
+++ marks.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int marks;
+       cout << "Enter marks";
+        cin >> marks;
+
+     if(marks>=90){
+        cout << "A";
+     }else if (marks>=80 && marks<90){
+        cout << "B";
+     }else{
+        cout << "C";
+     }
+    
+        return 0;
+}
