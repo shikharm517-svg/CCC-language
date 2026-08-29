afd0c3ebbd86822856efe8a6e60a40fc5caa6e72
Shikhar Mishra <shikharm517@gmail.com>
2026-08-29 13:38:16 +0530

    update

---
+++ while1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int count = 1;
+
+    while (count<=5) {
+
+        cout << count << " ";
+        count ++;
+    }
+     cout << endl;
+     return 0;
+
+}
